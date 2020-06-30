9519b6c04a3152ba342e4960e0927564fd43756d
AlekseyVY <alekseyvy@gmail.com>
2020-06-30 16:01:30 +0400

    bootcamp project

---
+++ Software_Engineering_Foundations/bootcamp_project/lib/bootcamp.rb
@@ -0,0 +1,72 @@
+class Bootcamp
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new { |hash, k| hash[k] = []}
+  end
+
+  def name
+    @name
+  end
+
+  def slogan
+    @slogan
+  end
+
+  def teachers
+    @teachers
+  end
+
+  def students
+    @students
+  end
+
+  def hire(teacher)
+    @teachers << teacher
+  end
+
+  def enroll(student)
+    if @students.length < @student_capacity
+        @students << student
+        return true
+    else
+        return false
+    end
+    end
+
+    def enrolled?(student)
+        if @students.include?(student)
+            return true
+        else
+            return false
+        end
+    end
+
+    def student_to_teacher_ratio
+        @students.length / @teachers.length
+    end
+
+    def add_grade(student, grade)
+        if enrolled?(student)
+            @grades[student] << grade
+            return true
+        else
+            return false
+        end
+    end
+
+    def num_grades(student)
+        @grades[student].length
+    end
+
+    def average_grade(student)
+        if @grades[student].length != 0 && enrolled?(student)
+            return @grades[student].sum / @grades[student].length
+        else
+            return nil
+        end
+    end
+end
