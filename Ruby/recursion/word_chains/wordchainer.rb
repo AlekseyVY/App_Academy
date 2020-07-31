1e9565c0a38acb8a882007a36d27e575a8ede0fb
AlekseyVY <alekseyvy@gmail.com>
2020-07-31 16:16:56 +0000

    some fixes

--- Ruby/recursion/word_chains/wordchainer.rb
+++ Ruby/recursion/word_chains/wordchainer.rb
@@ -1,4 +1,26 @@
-# class WordChainer
-    
-    
-# end
+ require 'set'
+ 
+class WordChainer
+    attr_reader :dictionary
+
+    def initialize(dictionary_file_name)
+        file = File.open(dictionary_file_name)
+        @dictionary = file.readlines.map(&:chomp)
+        file.close
+    end
+
+    def adjacent_words(word)
+        s_1 = Set.new()
+        @dictionary.map do |ele|
+            if word.length == ele.length
+                s_1.add(ele)
+        end
+    end
+        s_1
+    end
+end
+
+
+
+# w = WordChainer.new("dict.txt")
+# p w.adjacent_words('cats')
