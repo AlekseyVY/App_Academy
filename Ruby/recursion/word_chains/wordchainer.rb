1083af8d5e7ddc745c232e14078431d4e8d35c43
AlekseyVY <alekseyvy@gmail.com>
2020-08-02 12:35:18 +0000

    changed proj

--- Ruby/recursion/word_chains/wordchainer.rb
+++ Ruby/recursion/word_chains/wordchainer.rb
@@ -14,9 +14,11 @@ class WordChainer
         @dictionary.map do |ele|
             if word.length == ele.length
                 s_1.add(ele)
+            end
         end
-    end
-        s_1
+        s_1.each_with_index do |ele, idx|
+            
+        end
     end
 end
 
@@ -24,3 +26,6 @@ end
 
 w = WordChainer.new("dict.txt")
 p w.adjacent_words('cats')
+
+
+# find every adjacent word of given length. with only one word at given index can be unequal.
