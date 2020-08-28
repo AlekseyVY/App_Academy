e3aa5187ff664ec360a13b3bd2846d7088c98e5b
AlekseyVY <alekseyvy@gmail.com>
2020-08-28 08:23:44 +0000

    inheritance exercise

--- Javascript/proto_inheritance_exercises/inherits.js
+++ Javascript/proto_inheritance_exercises/inherits.js
@@ -1,1 +1,31 @@
-//make this inheritance
+Function.prototype.inherits = function(SuperClass){
+    let Surrogate = function() {}
+    Surrogate.prototype = SuperClass.prototype
+    this.prototype = new Surrogate()
+    this.prototype.constructor = this
+}
+
+function MovingObject() {}
+
+
+function Ship() {}
+Ship.inherits(MovingObject);
+
+function Asteroid() {}
+Asteroid.inherits(MovingObject);
+
+let astero = new Asteroid()
+let ship = new Ship()
+
+MovingObject.prototype.a = function() {
+    console.log('MOVINGOBJECT')
+}
+
+ship.a()
+astero.a()
+
+Asteroid.prototype.idle = function(){
+    console.log('I AM ASTEROID')
+}
+//ship.idle()
+astero.idle()
